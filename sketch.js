dad09112065f53b71149158bc812fe0b1b19ea52
sreevardhan37 <74353232+sreevardhan37@users.noreply.github.com>
2021-01-18 04:23:17 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+polygon = Bodies.circle(50,200,20);
+world.add(world,polygon);
+slingShot = new slingShot (this.polygon,{x:100,y:200});
+imgeMoade(CENTER)
+image(polgon_img,polygony.position.x,polygon.position.y,40,40);
